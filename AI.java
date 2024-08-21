7df4af04a76e2e52a9fbc5b9255f3ba639536fbc
Aayush Tewari <113117171+Aayushow1912@users.noreply.github.com>
2024-08-21 09:44:51 +0530

    Update AI.java

--- AI.java
+++ AI.java
@@ -1,5 +1,5 @@
-class AI
-    public static void main(String[] args) {
-        System.out.println("Hello AI");
+public class AI{
+    public static void main(String args[]){
+        System.out.print("Hello AI");
     }
 }
